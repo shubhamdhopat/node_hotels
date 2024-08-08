ac94577b13b6b6be62c83930e1972d3e900bdb8d
Shubham <shubhamdhopat@gmail.com>
2024-08-08 11:47:19 +0530

    updated changes

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const db = require('./db');  
+require('dotenv').config();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());//req.body
+const PORT=process.env.PORT || 3000;//IF PORT VALUE IS PRESENT IN process.env.port then it will use that or by default it will used 3000
 
 app.get('/', function (req, res) {
   res.send('Welcome to this hotel....,we have list of menus');
 });
+
 
 //Import the router file 
 const personRoutes=require('./routes/personRoutes');
@@ -16,6 +19,8 @@ const  menuItemRoutes=require('./routes/menuItemRoutes');
 app.use('/person', personRoutes);
 app.use('/menu', menuItemRoutes);
 
-app.listen(3000, () => {
+
+
+app.listen(PORT, () => {
   console.log("Server is running");
 });
